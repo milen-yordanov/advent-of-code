58511c47ee526cc667e4bf2cba3422b9bca081f1
Milen Yordanov <11314287+milenyordanov@users.noreply.github.com>
2018-12-03 11:58:09 +0200

    2018, day 3

---
+++ 2018/3/1.py
@@ -0,0 +1,37 @@
+# See: https://adventofcode.com/2018/day/3#part1
+
+input_file_name = 'input.txt'
+
+with open(input_file_name) as f:
+    content = f.readlines()
+
+def parse_claim(claim_str):
+    splits = claim_str.split()
+    stl = splits[2].split(',')
+    swh = splits[3].split('x')
+    return {
+        'top': int(stl[0]),
+        'left':  int(stl[1].split(':')[0]),
+        'width': int(swh[0]),
+        'height': int(swh[1])
+    }
+
+#1 @ 906,735: 28x17
+claims = [parse_claim(x.strip()) for x in content]
+
+w, h = 1500, 1500;
+matrix = [[0 for x in range(w)] for y in range(h)]
+
+print 'claims {}'.format(len(claims))
+for claim in claims:
+    for x in range(claim['left'], claim['left'] + claim['width']):
+        for y in range(claim['top'], claim['top'] + claim['height']):
+            matrix[x][y] += 1
+
+colisions = 0
+for x in range(w):
+    for y in range(h):
+        if matrix[x][y] > 1:
+            colisions += 1
+
+print colisions
---
+++ 2018/3/2.py
@@ -0,0 +1,10 @@
+# See: https://adventofcode.com/2018/day/3#part2
+
+input_file_name = 'input.txt'
+
+with open(input_file_name) as f:
+    content = f.readlines()
+
+content = [x.strip() for x in content] 
+
+# TODO
