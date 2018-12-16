2a741ea2f3b692e965ed33277922dc98afbe0702
Milen Yordanov <11314287+milenyordanov@users.noreply.github.com>
2018-12-16 16:16:03 +0200

    2018, day 4

--- 2018/4/1.py
+++ 2018/4/1.py
@@ -3,7 +3,7 @@
 import re
 import pprint
 
-input_file_name = 'input-test.txt'
+input_file_name = 'input.txt'
 
 with open(input_file_name) as f:
     content = f.readlines()
@@ -29,6 +29,50 @@ def parse_record(record_str):
     }
 
 records = [parse_record(x.strip()) for x in content]
-pprint.pprint(records)
-# print records
+records = sorted(records, key=lambda k: k['ts']) 
 
+
+guard_id = 0
+falls_asleep_min = 0
+guard_asleep = {}
+for rec in records:
+    if (guard_id == 0) and (rec['action'] != 'Guard'):
+        print ('Skip record {}'.format(rec))
+        continue
+
+    if rec['action'] == 'Guard':
+        guard_id = rec['guard_id']
+      
+    if rec['action'] == 'falls':
+        falls_asleep_min = rec['minute']
+
+    if rec['action'] == 'wakes':
+        wakes_up_min = rec['minute']
+        if not (guard_id in guard_asleep):
+            guard_asleep[guard_id] = []
+
+        guard_asleep[guard_id].append( (falls_asleep_min, wakes_up_min) )
+
+guard_asleep_total = []
+for guard_id in guard_asleep:
+    intervals = guard_asleep[guard_id]
+    total_asleep = 0
+    for interval in intervals:
+        total_asleep += interval[1] - interval[0]
+
+    guard_asleep_total.append({
+        'guard_id': guard_id,
+        'total_asleep': total_asleep,
+    })
+
+guard_asleep_total = sorted(guard_asleep_total, key=lambda k: k['total_asleep']) 
+most_asleep_guard_id = guard_asleep_total[-1]['guard_id']
+
+asleep_minutes_stats = [0 for x in range(60)]
+for interval in guard_asleep[most_asleep_guard_id]:
+    for i in range(interval[0], interval[1]):
+        asleep_minutes_stats[i] += 1
+
+asleep_most_during_minute = asleep_minutes_stats.index(max(asleep_minutes_stats))
+
+print (asleep_most_during_minute * most_asleep_guard_id)
