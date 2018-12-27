426fe85a161fc103fcfbf506e6ed57f30652402c
Milen Yordanov <11314287+milenyordanov@users.noreply.github.com>
2018-12-27 15:16:25 +0200

    2018, day 6

--- 2018/6/2.py
+++ 2018/6/2.py
@@ -3,4 +3,29 @@
 import re
 
 input_file_name = 'input.txt'
+TOTAL_DISTANCE = 10000
 
+with open(input_file_name) as f:
+    content = f.readlines()
+
+def parse_coordinate(coordinate_str):
+    toks = coordinate_str.split(',')
+    return (int(toks[0]),int(toks[1]))
+
+coordinates = [parse_coordinate(x.strip()) for x in content]
+
+max_x = max(coordinates, key=lambda k: k[0])[0] + 1
+max_y = max(coordinates, key=lambda k: k[1])[1] + 1
+
+def get_distance(x, y, coord):
+    return abs(x - coord[0]) + abs(y - coord[1])
+
+area_size = 0
+for x in range(max_x):
+    for y in range(max_y):
+        distances = [get_distance(x, y, coord) for coord in coordinates]
+        total_dist = sum(distances)
+        if total_dist < TOTAL_DISTANCE:
+            area_size += 1
+     
+print (area_size)
